1feed70c54d805aa0e363688a0fe86f72aacb52b
xudq <xudingqiang520@126.com>
2024-07-25 19:35:40 +0800

    finish calc ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,126 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QIcon con("D:\\code\\qt\\qt_calc\\back.png");
+    ui->backButton->setIcon(con);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_oneButton_clicked()
+{
+    expression += "1";
+    ui->lineEdit ->setText(expression);
+}
+
+
+void MainWindow::on_twoButton_clicked()
+{
+    expression += "2";
+    ui->lineEdit ->setText(expression);
+}
+
+
+void MainWindow::on_threeButton_clicked()
+{
+    expression += "3";
+    ui->lineEdit ->setText(expression);
+}
+
+
+void MainWindow::on_fourBtton_clicked()
+{
+    expression += "4";
+    ui->lineEdit ->setText(expression);
+}
+
+
+void MainWindow::on_fiveButton_clicked()
+{
+    expression += "5";
+    ui->lineEdit ->setText(expression);
+}
+
+
+void MainWindow::on_sixButton_clicked()
+{
+    expression += "6";
+    ui->lineEdit ->setText(expression);
+}
+
+
+void MainWindow::on_sevenButton_clicked()
+{
+    expression += "7";
+    ui->lineEdit ->setText(expression);
+}
+
+
+void MainWindow::on_eightButton_clicked()
+{
+    expression += "8";
+    ui->lineEdit ->setText(expression);
+}
+
+
+void MainWindow::on_nineButton_clicked()
+{
+    expression += "9";
+    ui->lineEdit ->setText(expression);
+}
+
+
+void MainWindow::on_zeroButton_clicked()
+{
+    expression += "0";
+    ui->lineEdit ->setText(expression);
+}
+
+
+void MainWindow::on_cleanButton_clicked()
+{
+    expression.clear();
+    ui->lineEdit->clear();
+}
+
+
+void MainWindow::on_addButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_subButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_mulButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_divButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_resultButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_backButton_clicked()
+{
+    expression.chop(1);
+    ui->lineEdit ->setText(expression);
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -17,7 +17,44 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_oneButton_clicked();
+
+    void on_twoButton_clicked();
+
+    void on_threeButton_clicked();
+
+    void on_fourBtton_clicked();
+
+    void on_fiveButton_clicked();
+
+    void on_sixButton_clicked();
+
+    void on_sevenButton_clicked();
+
+    void on_eightButton_clicked();
+
+    void on_nineButton_clicked();
+
+    void on_zeroButton_clicked();
+
+    void on_cleanButton_clicked();
+
+    void on_addButton_clicked();
+
+    void on_subButton_clicked();
+
+    void on_mulButton_clicked();
+
+    void on_divButton_clicked();
+
+    void on_resultButton_clicked();
+
+    void on_backButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QString expression ;
+
 };
 #endif // MAINWINDOW_H
